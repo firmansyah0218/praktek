eea332d0e954dba6abd8c8056042b99b64827573
firmansyah0218 <ferihabib96@gmail.com>
2017-11-07 21:05:11 +0800

    menghitung selisih absolute dari dua nila (X dan Y), di mana selisih dari (X-Y) akan selalu positif

--- praktek1.cpp
+++ praktek1.cpp
@@ -6,5 +6,31 @@ using namespace std;
 
 int main () {
 	
+	int x, y, z;
+	cout << "Input nilai x = ";
+	cin  >> x;
+	
+	cout << "Input nilai y = ";
+	cin  >> y;
+	
+	
+	if (x > y)	{
+		z = y - x;	
+		cout<<abs (z)<<endl;
+		cout<<"Hasil Positif";
+	}
+	
+	else if (y > x) {
+		z = x - y;
+		cout<<abs (z)<<endl;
+		cout<<"Hasil Positif";
+	}
+	
+		else if (z < 0) {
+		z = z * (-1) ;
+		cout<<abs (x-y)<<endl;
+		cout<<"Hasil Positif";
+	}
+	
 	}
 
