4d289be4b429e88670f20baffca4a496dca8ce98
firmansyah0218 <ferihabib96@gmail.com>
2017-11-08 09:58:58 +0800

    menghitung gaji bersih karyawan. Dikurangi 15% pajak untuk semua karyawan

---
+++ praktek3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+
+int main () {
+		int gp,jk,lembur,gt,pajak;
+	
+	cout << "Masukkan gaji pegawai : ";
+	cin >> gp;
+	cout << "Masukkan jam kerja    : ";
+	cin >> jk;
+	
+	lembur= jk - 40;
+	gt = (lembur*1.5+40)+gp;
+	pajak=gt*0.15;
+	gt=gt-pajak;
+	
+	cout<<"Gaji bersih pegawai : "<< gt<<endl;
+	
+}
